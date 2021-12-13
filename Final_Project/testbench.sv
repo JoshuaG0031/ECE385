5e3c46ce945bf1ad486423e2b960f65e2ae98c19
Boyan <Boyan.19@intl.zju.edu.cn>
2021-12-13 11:26:33 +0800

    test bench added

--- Final_Project/testbench.sv
+++ Final_Project/testbench.sv
@@ -8,23 +8,35 @@ timeprecision 1ns;
    // Internal variables
 	logic Clk;
 	logic Reset;
-	logic frame_clk;
-	logic [9:0] DrawX, DrawY;
-	logic w_key, a_key, d_key;
-	logic is_Fireboy;
-	logic [11:0] Fireboy_address;
+
+	logic       VGA_HS,      // Horizontal sync pulse.  Active low
+				   VGA_VS,      // Vertical sync pulse.  Active low
+				   VGA_CLK;     // 25 MHz VGA clock input
+	logic       VGA_BLANK_N, // Blanking interval indicator.  Active low.
+					VGA_SYNC_N;  // Composite Sync signal.  Active low.  We don't use it in this lab,
+                                                        // but the video DAC on the DE2 board requires an input for it.
+   logic [9:0] DrawX,       // horizontal coordinate  
+					DrawY;
+	
+
+	logic         w_key, a_key, d_key;
+	logic  is_Fireboy;           // Whether current pixel belongs to Fireboy
+	logic [11:0] Fireboy_address;	// return the character pixel adress for ROM inference
 	logic [3:0] Fireboy_direction;
+	logic frame_clk_delayed, frame_clk_rising_edge;
 	
-	// initialize the toplevel entity
-	Fireboy test(.*);
 	
+// initialize the toplevel entity
+	VGA_controller test(.Clk(Clk),.Reset(Reset),.VGA_HS(VGA_HS),.VGA_VS(VGA_VS),.VGA_CLK(VGA_CLK),.VGA_BLANK_N(VGA_BLANK_N),.VGA_SYNC_N(VGA_SYNC_N)
+	,.DrawX(DrawX),.DrawY(DrawY));
+	vga_clk vga_clk_instance(.inclk0(Clk), .c0(VGA_CLK));
+	Fireboy Fireboy_test(.*,.frame_clk(VGA_VS));
+	
+	assign frame_clk_delayed=Fireboy_test.frame_clk_delayed; 
+	assign frame_clk_rising_edge=Fireboy_test.frame_clk_rising_edge;
 	// set clock rule
    always begin : CLOCK_GENERATION 
 		#1 Clk = ~Clk;
-   end
-	
-	always begin : frame_clk_GENERATION 
-		#88 frame_clk = ~frame_clk;
    end
 	
 	// initialize clock signal 
@@ -35,18 +47,11 @@ timeprecision 1ns;
 	// begin testing
 	initial begin
 		Reset = 0; 
-		DrawX = 9'b0;
-		DrawY = 9'b0;
-		w_key = 1'b0; 
-		a_key = 1'b0;
-		d_key = 1'b0;
-	#2	Reset = 1; 
-	#2 Reset = 0; 		
-#30	w_key = 1;
-#30   w_key = 0;
-		d_key = 1;
-#30   d_key = 0;
-		a_key = 1;
+		w_key =0; 
+		a_key =1;
+		d_key =0;
+	#60	Reset = 1; 
+	#2 	Reset = 0;
 	end
 	 
 endmodule
