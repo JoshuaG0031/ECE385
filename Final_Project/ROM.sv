7d1d8a72762b5226c5bdfab6cd47582f2e504bca
Boyan <Boyan.19@intl.zju.edu.cn>
2021-12-13 21:56:01 +0800

    Improve python script for creating ROM module.

--- Final_Project/ROM.sv
+++ Final_Project/ROM.sv
@@ -4,40 +4,13 @@
  *
  */
 
-//module  ROM
-//(
-//		parameter size = 400, addrbit = 9,
-//		input [addrbit-1:0] read_address,
-//		output logic [23:0] color_out
-//);
-//
-//// mem has width of 1 Byte and a total of (size) addresses
-//logic [7:0] mem [0:size-1];
-//
-//initial
-//begin
-//	 $readmemh("sprite_bytes/tetris_I.txt", mem);
-//end
-//
-//
-//always_ff @ (posedge Clk) begin
-//	if (we)
-//		mem[write_address] <= data_In;
-//	data_Out<= mem[read_address];
-//end
-//
-//endmodule
-
-
-module  Fireboy_still
+module Fireboy_still
 (
-		input [11:0] read_address,
-		output logic [23:0] pixel_color
+                input [11:0] read_address,
+                output logic [23:0] pixel_color
 );
 
-// ram has width of 3 bits and a total of 400 addresses
 logic [3:0] ram [0:3599];
-
 logic [23:0] palette [10:0];
 assign palette[0] = 24'h800080;
 assign palette[1] = 24'h000000;
@@ -50,30 +23,22 @@ assign palette[7] = 24'h2C1007;
 assign palette[8] = 24'hA15427;
 assign palette[9] = 24'h6B4E2A;
 assign palette[10] = 24'h936450;
-//'0x800080', '0x000000', '0xEF2108', '0x61180C', '0x9E1609', '0xF84809', '0xF9F9F3', '0x2C1007', '0xA15427', '0x6B4E2A', '0x936450'
 assign pixel_color = palette[ram[read_address]];
 
 initial
 begin
-	 $readmemh("../PNG_To_Hex/On-Chip_Memory/sprite_bytes/Fireboy_still.txt", ram);
+        $readmemh("../PNG_To_Hex/On-Chip_Memory/sprite_bytes/Fireboy_still.txt",ram);
 end
 
 endmodule
 
-
-
-
-
-
-module  Fireboy_left
+module Fireboy_left
 (
-		input [11:0] read_address,
-		output logic [23:0] pixel_color
+                input [11:0] read_address,
+                output logic [23:0] pixel_color
 );
 
-// ram has width of 3 bits and a total of 400 addresses
 logic [3:0] ram [0:3599];
-
 logic [23:0] palette [10:0];
 assign palette[0] = 24'h800080;
 assign palette[1] = 24'h000000;
@@ -90,25 +55,18 @@ assign pixel_color = palette[ram[read_address]];
 
 initial
 begin
-	 $readmemh("../PNG_To_Hex/On-Chip_Memory/sprite_bytes/Fireboy_left.txt", ram);
+        $readmemh("../PNG_To_Hex/On-Chip_Memory/sprite_bytes/Fireboy_left.txt",ram);
 end
 
 endmodule
 
-
-
-
-
-
-module  Fireboy_right
+module Fireboy_right
 (
-		input [11:0] read_address,
-		output logic [23:0] pixel_color
+                input [11:0] read_address,
+                output logic [23:0] pixel_color
 );
 
-// ram has width of 3 bits and a total of 400 addresses
 logic [3:0] ram [0:3599];
-
 logic [23:0] palette [10:0];
 assign palette[0] = 24'h800080;
 assign palette[1] = 24'h000000;
@@ -121,12 +79,12 @@ assign palette[7] = 24'hFAF9F3;
 assign palette[8] = 24'hA04D25;
 assign palette[9] = 24'h6E4A2C;
 assign palette[10] = 24'hA19673;
-//'0x800080', '0x000000', '0xEF2108', '0x61180C', '0x9E1609', '0xF84809', '0xF9F9F3', '0x2C1007', '0xA15427', '0x6B4E2A', '0x936450'
 assign pixel_color = palette[ram[read_address]];
 
 initial
 begin
-	 $readmemh("../PNG_To_Hex/On-Chip_Memory/sprite_bytes/Fireboy_right.txt", ram);
+        $readmemh("../PNG_To_Hex/On-Chip_Memory/sprite_bytes/Fireboy_right.txt",ram);
 end
 
 endmodule
+
--- PNG_To_Hex/On-Chip_Memory/scripts/png_to_palette_relative_resizer.py
+++ PNG_To_Hex/On-Chip_Memory/scripts/png_to_palette_relative_resizer.py
@@ -3,6 +3,7 @@ from dominate import get_dominant_colors
 from collections import Counter
 from scipy.spatial import KDTree
 import numpy as np
+import math
 def hex_to_rgb(num):
     h = str(num)
     return int(h[0:4], 16), int(('0x' + h[4:6]), 16), int(('0x' + h[6:8]), 16)
@@ -15,13 +16,21 @@ image_path="../sprite_originals/" + filename+ ".png"
 color=get_dominant_colors(image_path)
 palette_hex=['0x800080']
 palette_hex.extend(color)
-print("---Copy the following code to the ROM module---")
-for i in range(len(palette_hex)):
-    print("assign palette[",i,"] = 24\'h",palette_hex[i][2:],";",sep='')
 new_w, new_h = map(int, input("What's the new height x width? Like 28 28. ").split(' '))
 # palette_hex = ['0x800080','0x000000', '0x15ADE9', '0x9AD9E9', '0xF0F2EE', '0x6193A3', '0x63D6F3', '0x1C6997']
+pix_num = int(new_w) * int(new_h)
+address_len = int(math.log2(pix_num))
+print("\n---Copy the following code to the ROM module---\n")
+print("module", filename)
+print("(\n\t\tinput [",address_len,":0] read_address,\n\t\toutput logic [23:0] pixel_color\n);\n",sep="")
+print("logic [3:0] ram [0:",pix_num-1,"];",sep="")
+print("logic [23:0] palette [10:0];")
+for i in range(len(palette_hex)):
+    print("assign palette[",i,"] = 24\'h",palette_hex[i][2:],";",sep='')
+print("assign pixel_color = palette[ram[read_address]];")
+print("\ninitial\nbegin\n\t$readmemh(\"../PNG_To_Hex/On-Chip_Memory/sprite_bytes/",filename,".txt\",ram);",sep="")
+print("end\n\nendmodule")
 palette_rgb = [hex_to_rgb(color) for color in palette_hex]
-
 pixel_tree = KDTree(palette_rgb)
 im = Image.open(image_path) #Can be many different formats.
 im = im.convert("RGBA")
